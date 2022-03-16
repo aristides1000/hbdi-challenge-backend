6d1c9d3274c5a15134b31854a2381a2106acb4db
aristides1000 <aristides1000@gmail.com>
2022-03-16 14:29:14 -0400

    Add find functionalitie

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,5 +1,10 @@
 class ListsController < ApplicationController
   def index
-    render json: List.all
+    render json: List.all, status: :ok and return if params[:name].blank?
+
+    @list = List.find_by_name(params[:name])
+    render json: {}, status: :not_found and return if @list.blank?
+
+    render json: { list: @list }, status: :ok
   end
 end
