175f51d730045e25164109d205e1852d40ff6fc4
aristides1000 <aristides1000@gmail.com>
2022-03-24 17:03:34 -0400

    Finish task

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,5 @@
 class ListsController < ApplicationController
   def index
-    render json: List.all, status: :ok and return if params[:name].blank?
-
-    @list = List.find_by_name(params[:name])
-    render json: {}, status: :not_found and return if @list.blank?
-
     render json: { list: @list }, status: :ok
   end
 end
